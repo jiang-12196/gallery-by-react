5f176d03f1347e0a36fee804b1cd7e36269c2bc2
jiang12196 <jiang12196@126.com>
2016-12-11 16:30:46 +0800

    add style

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,18 +11,39 @@ let yeomanImage = require('../images/3.png');
 //获取图片地址
 function genImageURL(imageDatasArr) {
   return  imageDatasArr.map(function (data) {
-          return require('../images/'+data.fileName)
+          data['imgURL'] =  require('../images/'+data.fileName);
+          return data;
   });
 }
 imageDatas = genImageURL(imageDatas);
 
+var ImgFigure = React.createClass ({
+  render : function () {
+    return(
+      <figure className="img-figure">
+        <img src={this.props.data.imgURL}
+              alt={this.props.data.title}/>
+        <figcaption>
+          <h2 className="img-title">{this.props.data.title}</h2>
+        </figcaption>
+      </figure>
+    )
+  }
+});
+
 
 class AppComponent extends React.Component {
   render() {
+    let controllerUnits = [],
+        imgFigures = [];
+    imageDatas.forEach(function (value) {
+      imgFigures.push(<ImgFigure data={value}/>)
+      }
+    )
     return (
       <section className="stage">
         <section className="img-sec">
-
+          {imgFigures}
         </section>
         <nav className="controller-nav">
 
